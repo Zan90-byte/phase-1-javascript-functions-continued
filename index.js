0a8d3371029161119783b7dae239301da59b8735
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-10-30 15:55:29 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//console.log(saturdayFun("dance"));
+
+//******************************************* */
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+//console.log(mondayWork());
+
+//***************************************** */
+
+function wrapAdjective(string = "*"){
+    return function (adjective = "special"){
+        return `You are ${string}${adjective}${string}!`;
+    }
+}
+//console.log(wrapAdjective("*")("a hard worker"));
